81cfe24b7db40d0de6358bc468c7ca9a90c13801
saturnian99 <saturnian.dsg@gmail.com>
2020-05-13 17:23:26 -0300

    Implemented 'animalsByIds' function.

--- src/zoo.js
+++ src/zoo.js
@@ -11,56 +11,45 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  if (!ids) return [];
+  return data.animals.filter(animal => ids.includes(animal.id));
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
 }
 
 function isManager(id) {
-  // seu código aqui
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
 }
 
 function animalCount(species) {
-  // seu código aqui
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
 }
 
 function animalMap(options) {
-  // seu código aqui
 }
 
 function schedule(dayName) {
-  // seu código aqui
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
 }
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
 }
 
 module.exports = {
