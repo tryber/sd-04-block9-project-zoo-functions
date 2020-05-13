7093c1bcfa3261675d1531dabcdc88008470887b
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-05-13 18:05:55 -0300

    employeeByName

--- src/zoo.js
+++ src/zoo.js
@@ -24,6 +24,9 @@ function animalsOlderThan(animal, age) {
 
 function employeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+  return data.employees.find(element => (
+  element.firstName === employeeName || element.lastName === employeeName));
 }
 
 function createEmployee(personalInfo, associatedWith) {
