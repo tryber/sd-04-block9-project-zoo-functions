ea914b4f6ab3c488b5c0da24884068eea0b9a656
Josias Viveiro <josiasviveiro@gmail.com>
2020-05-13 16:58:01 -0300

    animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -11,14 +11,10 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids = []) {
-  // seu código aqui
-  // Caso receba nenhum parâmetro, necessário retornar um array vazio
-  // Ao receber como parâmetro um único id, retorna os animais com este id
-  // Ao receber mais de um id, retorna os animais que têm um desses ids
-  return ids;
-
-}
+const animalsByIds = (...ids) => {
+  if (!ids) return [];
+  return data.animals.filter(element => ids.includes(element.id));
+};
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
