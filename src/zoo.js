1406b5f4b5230a2f96d5b30ec0412eddff2e9f16
matheusysd <matheusyurisd@gmail.com>
2020-05-13 17:53:01 -0300

    Função oldestFromFirstSpecies implementada

--- src/zoo.js
+++ src/zoo.js
@@ -74,9 +74,10 @@ function schedule(dayName) {
   // seu código aqui
 }
 
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const oldestFromFirstSpecies = id => Object.values(data.animals
+  .find(e => e.id === data.employees
+    .find(animal => animal.id === id).responsibleFor[0]).residents
+  .sort((anim1, anim2) => anim2.age - anim1.age)[0]);
 
 const increasePrices = percentage =>
   Object.keys(data.prices).forEach((price) => {
