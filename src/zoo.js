34eadd8f311b2408f1b5d79525cb5a054b014832
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-05-13 17:37:16 -0300

    requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -18,6 +18,8 @@ function animalsByIds(...ids) {
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  return data.animals.find(elemento => elemento.name === animal)
+  .residents.every(elemento => elemento.age >= age);
 }
 
 function employeeByName(employeeName) {
