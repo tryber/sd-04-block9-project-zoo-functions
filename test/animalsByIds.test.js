6e3f7152677c7f40c0151f9598952799f0eb1a06
djonata-dias <diasdjonata@gmail.com>
2020-05-13 20:27:30 -0300

    animalsByIds.test.js refatorado conforme requisitado pelo codeclimate

--- test/animalsByIds.test.js
+++ test/animalsByIds.test.js
@@ -8,12 +8,7 @@ describe('animalsByIds', () => {
     // sem parâmetros, retorna um array vazio
     actual = zoo.animalsByIds();
     expected = [];
-
-    assert.deepEqual(actual, expected);
-
-    // com um único id, retorna os animais com este id
-    actual = zoo.animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
-    expected = [{
+    const lions = [{
       id: '0938aa23-f153-4937-9f88-4858b24d6bce',
       name: 'lions',
       popularity: 4,
@@ -24,24 +19,19 @@ describe('animalsByIds', () => {
         { name: 'Faustino', sex: 'male', age: 7 },
         { name: 'Dee', sex: 'female', age: 14 }
       ]
-    }];
+
+    assert.deepEqual(actual, expected);
+
+    // com um único id, retorna os animais com este id
+    actual = zoo.animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
+    expected = lions;
 
     assert.deepEqual(actual, expected);
 
     // com mais de um id, retorna os animais que têm um desses ids
     actual = zoo.animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
       'e8481c1d-42ea-4610-8e11-1752cfc05a46');
-    expected = [{
-      id: '0938aa23-f153-4937-9f88-4858b24d6bce',
-      name: 'lions',
-      popularity: 4,
-      location: 'NE',
-      residents: [
-        { name: 'Zena', sex: 'female', age: 12 },
-        { name: 'Maxwell', sex: 'male', age: 15 },
-        { name: 'Faustino', sex: 'male', age: 7 },
-        { name: 'Dee', sex: 'female', age: 14 }
-      ]
+    expected = lions
     }, {
       id: 'e8481c1d-42ea-4610-8e11-1752cfc05a46',
       name: 'tigers',
