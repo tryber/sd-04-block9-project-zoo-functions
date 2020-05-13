49223874fb8a5f382dc91d545412c5d2563c263b
Rangel Paolo <paolorangel1996@outlook.com>
2020-05-13 20:30:03 -0300

    Issues fixed.

--- src/zoo.js
+++ src/zoo.js
@@ -34,7 +34,7 @@ const addEmployee = (id, firstName, lastName, managers = [], responsibleFor = []
   data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 
 const animalCount = (species = '') => {
-  return (species)? 
+  return (species) ?
   data.animals.find(a => a.name === species).residents.length:
   data.animals.reduce((r, a) => {
     r[a.name] = a.residents.length;
