3deda8b859afefb0e55388fce4b202a843391102
juniomelos <you@example.com>
2020-05-13 17:26:05 -0300

    Requirement: animalsOlderThan - CC issues

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require("./data");
+const data = require('./data');
 
 //  Implemente a função animalsByIds:
 //  Caso receba nenhum parâmetro, necessário retornar um array vazio
@@ -17,15 +17,15 @@ const data = require("./data");
 //  Ao receber mais de um id, retorna os animais que têm um desses ids
 
 const animalsByIds = (...ids) =>
-  data.animals.filter((animal) => ids.includes(animal.id));
+  data.animals.filter(animal => ids.includes(animal.id));
 
 //  Implemente a função animalsOlderThan:
 //  Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada
 
 const animalsOlderThan = (animal, age) =>
   data.animals
-    .find((beast) => beast.name === animal)
-    .residents.every((idade) => idade.age > age);
+    .find(beast => beast.name === animal)
+    .residents.every(idade => idade.age > age);
 
 function employeeByName(employeeName) {
   // seu código aqui
