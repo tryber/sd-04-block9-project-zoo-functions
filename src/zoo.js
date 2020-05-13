257d0eaeca08d8faaac49693ab3c66efdbe6f200
jessica-tavares <jessicatavareseel@gmail.com>
2020-05-13 18:19:03 -0300

    exercicio 6

--- src/zoo.js
+++ src/zoo.js
@@ -44,8 +44,8 @@ function isManager(id) {
   return data.employees.some(elem => elem.managers.some(ids => ids === id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees.push({id, firstName,lastName, managers, responsibleFor})
 }
 
 function animalCount(species) {
