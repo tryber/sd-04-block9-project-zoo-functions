a57600205073142cde56eee6163818a6efc88032
Leandro Marten <leandromarten1@gmail.com>
2020-05-14 14:18:41 -0300

    Fix CC2

--- src/zoo.js
+++ src/zoo.js
@@ -58,10 +58,10 @@ function animalCount(species) {
   // if(!species) return data.animals.map(e => `${e.name}: ${e.popularity}`);
   // return data.animals.filter(e => {if (e.name === species){ return e.popularity}})[0].popularity;
   if (!species) { 
-      return data.animals.reduce(function (total, { name, residents }) {
-      total[name] = residents.length;
-      return total;
-    }, {});
+    return data.animals.reduce(function (total, { name, residents }) {
+    total[name] = residents.length;
+    return total;
+  }, {});
   }
   return data.animals.find(({ name }) => name === species).residents.length;
 }
