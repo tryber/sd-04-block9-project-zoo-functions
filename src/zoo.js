f40777d9d16427c5ba24b14f12ca46e5508450f7
Rangel Paolo <paolorangel1996@outlook.com>
2020-05-14 14:54:18 -0300

    Fixing issues.

--- src/zoo.js
+++ src/zoo.js
@@ -87,10 +87,10 @@ const oldestFromFirstSpecies = (id) => {
 };
 
 const increasePrices = (percentage) => {
-  Object.keys(data.prices).forEach(chave =>  {
-    data.prices[chave] = parseFloat((data.prices[chave] + 
-      data.prices[chave] * ((percentage + 0.01) / 100) )
-    .toFixed(2))
+  Object.keys(data.prices).forEach((chave) => {
+    data.prices[chave] = parseFloat((data.prices[chave] +
+    data.prices[chave] * ((percentage + 0.01) / 100))
+    .toFixed(2));
   });
 };
 
