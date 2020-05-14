5071886146ad7b7ca3ad7606a0c8e472e614de35
orlando-messias <programadorlando@gmail.com>
2020-05-14 14:25:43 -0300

    cc issues

--- src/zoo.js
+++ src/zoo.js
@@ -116,9 +116,8 @@ function oldestFromFirstSpecies(id) {
 
 function increasePrices(percentage) {
   // seu código aqui
-  return Object.keys(data.prices).forEach((key) =>
-  (data.prices[key] = Math.round(data.prices[key] * 
-  ((percentage / 100) + data.prices[key]) * 100) / 100));
+  return Object.keys(data.prices).forEach((key) => (data.prices[key] =
+    Math.round(data.prices[key] * ((percentage / 100) + data.prices[key]) * 100) / 100));
 }
 
 function responsible(employee) {
