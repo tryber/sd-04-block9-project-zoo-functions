50b29a1864cd59371fc63eadc5bc16acfc955d5d
Leandro Marten <leandromarten1@gmail.com>
2020-05-14 16:13:55 -0300

    schedule() - OK

--- src/zoo.js
+++ src/zoo.js
@@ -86,9 +86,21 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // seu código aqui
-
+  const operation = {
+    'Tuesday': 'Open from 8am until 6pm',
+    'Wednesday': 'Open from 8am until 6pm',
+    'Thursday': 'Open from 10am until 8pm',
+    'Friday': 'Open from 10am until 8pm',
+    'Saturday': 'Open from 8am until 10pm',
+    'Sunday': 'Open from 8am until 8pm',
+    'Monday': 'CLOSED'
+  };
+  if (!dayName) return operation;
+  return {[dayName]: operation[dayName]};
+  
 }
+console.log(schedule());
+
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
