6c919a83243435d59bb3a18a9b5169e3e98e4a42
TerezaCarvalho <terezasimoes@gmail.com>
2020-05-14 17:45:46 -0300

    createEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -11,34 +11,68 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(...ids) {
-  // seu código aqui
-  return data.animals.filter(element => ids.includes(element.id));
-}
-  // console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+// function animalsByIds(...ids) {
+//   // seu código aqui
+//   return data.animals.filter(element => ids.includes(element.id));
+// }
+//   // console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
-function animalsOlderThan(animal, age) {
-  // seu código aqui
-  return data.animals.find(element => animal.includes(element.name))
-  // o find() me retorna o array do animal passado como parametro
-  // o loop ocorre só dentro desse array.
-                      .residents.every(element => element.age >= age);
-}
+// function animalsOlderThan(animal, age) {
+//   // seu código aqui
+//   return data.animals.find(element => animal.includes(element.name))
+//   // o find() me retorna o array do animal passado como parametro
+//   // o loop ocorre só dentro desse array.
+//                       .residents.every(element => element.age >= age);
+// }
 
-// console.log(animalsOlderThan('penguins', 10));
+// // console.log(animalsOlderThan('penguins', 10));
 
 
-function employeeByName(employeeName) {
-  // seu código aqui
-  return data.employees.find(
-    employee => employee.firstName === employeeName ||
-    employee.lastName === employeeName) || {};
-}
-// console.log(employeeByName('Emery'));
+// function employeeByName(employeeName) {
+//   // seu código aqui
+//   return data.employees.find(
+//     employee => employee.firstName === employeeName ||
+//     employee.lastName === employeeName) || {};
+// }
+// // console.log(employeeByName('Emery'));
+
+const personalInfo = {
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+};
+const associatedWith = {
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'
+  ],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+  ]
+};
+
+expected = {
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'
+  ],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+  ]
+};
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return {...personalInfo, ...associatedWith};
 }
+ //console.log(createEmployee(personalInfo, associatedWith));
 
 function isManager(id) {
   // seu código aqui
