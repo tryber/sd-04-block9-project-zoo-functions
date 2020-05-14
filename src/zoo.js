d1add9537ddbfb1a4bed75d58dc45d381a6652db
jessica-tavares <jessicatavareseel@gmail.com>
2020-05-14 16:11:56 -0300

    luta com o cc

--- src/zoo.js
+++ src/zoo.js
@@ -14,7 +14,8 @@ const data = require('./data');
 
 function animalsByIds(...ids) {
   const animals = data.animals.filter((elem) =>
-    ids.find((id) => id === elem.id));
+    ids.find((id) => id === elem.id)
+  );
   return animals;
 }
 
@@ -26,7 +27,11 @@ function animalsOlderThan(animal, age) {
 function employeeByName(employeeName) {
   if (!employeeName) return {};
   return data.employees.find(
-    (elem) => elem.firstName === employeeName || elem.lastName === employeeName);
+    (elem) =>
+      elem.firstName === employeeName ||
+      elem.lastName === employeeName ||
+      elem.id === employeeName
+  );
 }
 
 function createEmployee(personalInfo, associatedWith) {
@@ -44,7 +49,7 @@ function addEmployee(
   firstName,
   lastName,
   managers = [],
-  responsibleFor = [],
+  responsibleFor = []
 ) {
   data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
@@ -53,7 +58,8 @@ function animalCount(species) {
   if (!species) {
     const result = {};
     data.animals.forEach(
-      (objeto) => (result[objeto.name] = objeto.residents.length));
+      (objeto) => (result[objeto.name] = objeto.residents.length)
+    );
     return result;
   }
   return data.animals.find((animal) => animal.name === species).residents
@@ -64,7 +70,10 @@ function entryCalculator(entrants) {
   if (entrants && Object.keys(entrants).length > 0) {
     const chaves = Object.keys(entrants);
     return chaves.reduce(
-      (valorTotal, pessoas) => valorTotal + (data.prices[pessoas] * entrants[pessoas]), 0);
+      (valorTotal, pessoas) =>
+        valorTotal + data.prices[pessoas] * entrants[pessoas],
+      0
+    );
   }
   return 0;
 }
@@ -88,22 +97,51 @@ function schedule(dayName) {
 }
 
 function oldestFromFirstSpecies(id) {
-  const idAnimal = data.employees.find(empId => empId.id === id).responsibleFor[0];
-  const animal = data.animals.find(animais => animais.id === idAnimal);
+  const idAnimal = data.employees.find((empId) => empId.id === id)
+    .responsibleFor[0];
+  const animal = data.animals.find((animais) => animais.id === idAnimal);
   return Object.values(animal.residents.sort((a, b) => b.age - a.age)[0]);
 }
 
 function increasePrices(percentage) {
-  const aumento = (percentage / 100) + 1;
+  const aumento = percentage / 100 + 1;
   Object.keys(data.prices).forEach((price) => {
-    (data.prices[price] = Math.round((data.prices[price] * aumento) * 100) / 100);
+    data.prices[price] = Math.round(data.prices[price] * aumento * 100) / 100;
   });
   // parseFloat((0.2 + 0.1).toPrecision(2))
 }
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
+  const allEmployees = gerandoEmployeesArray();
+  const oneEmployee = {};
+  if (!idOrName) return allEmployees;
+  if (idOrName) {
+    const employeeElement = employeeByName(idOrName);
+    const name = `${employeeElement.firstName} ${employeeElement.lastName}`;
+    oneEmployee[name] = allEmployees[name];
+    return oneEmployee;
+  }
 }
+
+const gerandoEmployeesArray = () => {
+  const result = {};
+  for (let i = 0; i < data.employees.length; i += 1) {
+    let name = `${data.employees[i].firstName} ${data.employees[i].lastName}`;
+    result[name] = arrayDeAnimais(data.employees[i].id);
+  }
+  return result;
+};
+
+const arrayDeAnimais = (infoId) => {
+  const animaisId = data.employees.find((elem) => elem.id === infoId)
+    .responsibleFor;
+  let arrayNomes = [];
+  for (let i = 0; i < animaisId.length; i += 1) {
+    arrayNomes.push(data.animals.find((elem) => elem.id === animaisId[i]).name);
+  }
+  //const arrayNomes = data.animals.filter(elem => elem.id === animaisId.forEach(index => index)).name;
+  return arrayNomes;
+};
 
 module.exports = {
   entryCalculator,
