7916adb7eac94b6f353d79592ad090facaf83050
Rangel Paolo <paolorangel1996@outlook.com>
2020-05-13 22:26:01 -0300

    entryCalculator passed.

--- src/zoo.js
+++ src/zoo.js
@@ -49,9 +49,17 @@ const animalCount = (species = '') => {
   }, {});
 };
 
-console.log(animalCount('snakes'));
-const entryCalculator = (entrants) => {};
+const entryCalculator = (entrants = '') => {
+  if (!Object.entries(entrants).length) return 0;
+  const coust = Object.values(data.prices);
+  // console.log(coust);
+  return Object.entries(entrants).
+    reduce((r, v, i) => r + (v[1] * coust[i]), 0) - 8;
+};
 
+let b = { 'Adult': 2, 'Child': 3, 'Senior': 1 };
+console.log(entryCalculator(b));
+// console.log(Object.entries(b).length);
 function animalMap(options) {
   // seu código aqui
 }
