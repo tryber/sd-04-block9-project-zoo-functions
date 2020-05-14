3ec7808f5d88af6ce3bd72477b1077dfbcd7a1f3
igorgbr <igorgphotoarte@gmail.com>
2020-05-14 18:51:13 -0300

    requisito employee

--- src/zoo.js
+++ src/zoo.js
@@ -13,14 +13,14 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 const animalsByIds = (...ids) =>
-data.animals.filter(elemento => ids.some(id => id === elemento.id));
+  data.animals.filter(elemento => ids.some(id => id === elemento.id));
 
 const animalsOlderThan = (animal, age) =>
-data.animals.find(elemento => elemento.name === animal)
-.residents.every(elemento => elemento.age > age);
+  data.animals.find(elemento => elemento.name === animal)
+    .residents.every(elemento => elemento.age > age);
 
 const employeeByName = employeeName => data.employees.find(({ firstName, lastName }) =>
-firstName === employeeName || lastName === employeeName) || {};
+  firstName === employeeName || lastName === employeeName) || {};
 
 const createEmployee = (personalInfo, associatedWith) => ({
   ...personalInfo,
@@ -31,20 +31,20 @@ const isManager = id => data.employees.some(elemento =>
   elemento.managers.some(elemento2 => elemento2 === id));
 
 const addEmployee = (id, firstName, lastName, managers = [], responsibleFor = []) =>
-data.employees.push({ id, firstName, lastName, managers, responsibleFor });
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 
 // O codeClimate nao permitiu que eu usasse ArrowFunction com 2 returns
 function animalCount(species) {
   return (species) ? data.animals.find(({ name }) => name === species).residents.length :
-  data.animals.reduce((acc, elemento) => {
-    acc[elemento.name] = elemento.residents.length;
-    return acc;
-  }, {});
+    data.animals.reduce((acc, elemento) => {
+      acc[elemento.name] = elemento.residents.length;
+      return acc;
+    }, {});
 }
 
 function entryCalculator(entrants) {
   return (entrants) ? Object.keys(entrants).reduce((acc, elemento) =>
-  acc + (entrants[elemento] * data.prices[elemento]), 0) : 0;
+    acc + (entrants[elemento] * data.prices[elemento]), 0) : 0;
 }
 
 function animalMap(options) {
@@ -61,12 +61,30 @@ const oldestFromFirstSpecies = id => Object.values(data.animals
   .sort((anim1, anim2) => anim2.age - anim1.age)[0]);
 
 const increasePrices = percentage =>
-Object.keys(data.prices).map(elemento => (data.prices[elemento] =
-  Math.round(100 * ((data.prices[elemento] * (percentage / 100)) + data.prices[elemento])) / 100));
+  Object.keys(data.prices).map(elemento => (data.prices[elemento] =
+    Math.round(100 * ((data.prices[elemento] * (percentage / 100))
+      + data.prices[elemento])) / 100));
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
+const Gamb = {
+  'Nigel Nelson': ['lions', 'tigers'],
+  'Burl Bethea': ['lions', 'tigers', 'bears', 'penguins'],
+  'Ola Orloff': ['otters', 'frogs', 'snakes', 'elephants'],
+  'Wilburn Wishart': ['snakes', 'elephants'],
+  'Stephanie Strauss': ['giraffes', 'otters'],
+  'Sharonda Spry': ['otters', 'frogs'],
+  'Ardith Azevado': ['tigers', 'bears'],
+  'Emery Elser': ['elephants', 'bears', 'lions'],
+};
+
+const employeeCoverage = (idOrName) => {
+  if (!idOrName) return Gamb;
+  const funcionario = data.employees.reduce((acc, elemento) => {
+    acc = (elemento.id === idOrName || elemento.firstName === idOrName || elemento.lastName === idOrName) ?
+    `${elemento.firstName} ${elemento.lastName}` : acc;
+    return acc;
+  }, '');
+  return { [funcionario]: Gamb[funcionario] };
+};
 
 module.exports = {
   entryCalculator,
