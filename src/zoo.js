5cac19b7ac30c0f43807a421821f7ac0f1e2aa44
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-05-14 00:08:41 -0300

    1 to 9

--- src/zoo.js
+++ src/zoo.js
@@ -11,41 +11,89 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+const animalsByIds = (...ids) => data.animals.filter(animal => ids.some(id => animal.id === id))
+
+const animalsOlderThan = (animalName, age) => data.animals.find(animal => animalName === animal.name).residents.every(resident => resident.age >= age)
+
+const employeeByName = (employeeName) => {
+  const employeeFiltered = data.employees.find(employee => employee.firstName === employeeName || employee.lastName === employeeName)
+  return {
+    ...employeeFiltered
+  }
 }
 
-function animalsOlderThan(animal, age) {
-  // seu código aqui
+const createEmployee = ({ id, firstName, lastName }, { managers, responsibleFor }) => ({
+  id,
+  firstName,
+  lastName,
+  managers,
+  responsibleFor,
+})
+
+const isManager = (id) => data.employees.some(employee => employee.managers.some(maneger => maneger === id))
+
+const addEmployee = (id, firstName, lastName, managers = [], responsibleFor = []) => {
+  const employee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  }
+  data.employees.push(employee)
 }
 
-function employeeByName(employeeName) {
-  // seu código aqui
+const animalCount = (species = "all") => {
+  if (species === "all") {
+    let animalsNumbers = {};
+    data.animals.forEach(({ name, residents }) => {
+      animalsNumbers = {
+        ...animalsNumbers,
+        [name]: residents.length
+      }
+    }
+    )
+    return animalsNumbers
+  }
+  return data.animals.find(specie => specie.name === species).residents.length
 }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+const entryCalculator = ({ Adult = 0, Child = 0, Senior = 0 }) => Adult * data.prices.Adult + Child * data.prices.Child + Senior * data.prices.Senior
+
+//funções auxiliares para a animalMap
+
+const filter = (location) => data.animals.filter(animal => animal.location === location)
+
+const getName = (arr) => arr.map(animal => animal.name)
+
+//gambiarra feita para filtrar por sexo sem alterar muito as funções, precisa ser refatorada
+
+const findAndMapResidents = (name, sex) => sex ? data.animals.find(animal => animal.name === name).residents.filter(resident => resident.sex === sex).map(resident => resident.name) : data.animals.find(animal => animal.name === name).residents.map(resident => resident.name)
+
+const getResidents = (names, includeNames, sorted, sex) => {
+  let residentsObj = []
+  if (includeNames) {
+    names.forEach(name => {
+      residentsObj = [
+        ...residentsObj,
+        { [name]: sorted ? findAndMapResidents(name, sex).sort() : findAndMapResidents(name, sex) }
+      ]
+    })
+    return residentsObj
+  }
+  return names
 }
 
-function isManager(id) {
-  // seu código aqui
+const animalMap = (options) => {
+  const {includeNames, sorted, sex} = options ? options : {includeNames: false, sorted: false, sex: false}
+  const locations = ["NE", "NW", "SE", "SW"]
+  let animalLocations = {}
+  locations.forEach(location => {
+    animalLocations[location] = getResidents(getName(filter(location)), includeNames, sorted, sex)
+  })
+  return animalLocations
 }
-
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
-
-function animalCount(species) {
-  // seu código aqui
-}
-
-function entryCalculator(entrants) {
-  // seu código aqui
-}
-
-function animalMap(options) {
-  // seu código aqui
-}
+options = { includeNames: true }
 
 function schedule(dayName) {
   // seu código aqui
