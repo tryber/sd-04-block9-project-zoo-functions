37c2f7d1112a8637cf26ff169396e777295bb814
igorgbr <igorgphotoarte@gmail.com>
2020-05-14 18:55:40 -0300

    requisito employee CC 2

--- src/zoo.js
+++ src/zoo.js
@@ -79,7 +79,7 @@ const Gamb = {
 const employeeCoverage = (idOrName) => {
   if (!idOrName) return Gamb;
   const funcionario = data.employees.reduce((acc, elemento) => {
-    acc = (elemento.id === idOrName || 
+    acc = (elemento.id === idOrName ||
       elemento.firstName === idOrName || elemento.lastName === idOrName) ?
     `${elemento.firstName} ${elemento.lastName}` : acc;
     return acc;
