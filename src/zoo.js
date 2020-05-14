ebb03f0a67ff79d209f5107c340b1baa322f3798
AliceAAtalla <aliceatalla36@gmail.com>
2020-05-14 17:07:26 -0300

    Requisito 3 - Funçao EmployeeByName

--- src/zoo.js
+++ src/zoo.js
@@ -11,21 +11,25 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-//TODO => 1 - Caso receba nenhum parâmetro, necessário retornar um array vazio;
-//TODO => 2 - Ao receber como parâmetro um único id, retorna os animais com este id;
-//TODO => 3 - Ao receber mais de um id, retorna os animais que têm um desses ids;
+// TODO => 1 - Caso receba nenhum parâmetro, necessário retornar um array vazio;
+// TODO => 2 - Ao receber como parâmetro um único id, retorna os animais com este id;
+// TODO => 3 - Ao receber mais de um id, retorna os animais que têm um desses ids;
 const animalsByIds = (...ids) => {
-  return data.animals.filter(animal => ids.some(id => animal.id === id))
+  return data.animals.filter(animal => ids.some(id => animal.id === id));
 }
 
-//TODO => Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada
+// TODO => Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada
 const animalsOlderThan = (animal, age) => {
   return data.animals.find(especie => especie.name === animal)
-  .residents.every(residente => residente.age > age)
+    .residents.every(residente => residente.age >= age);
 }
 
-function employeeByName(employeeName) {
-  // seu código aqui
+// TODO => 1 - Sem parâmetros, retorna um objeto vazio
+// TODO => 2 - Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
+// TODO => 3 - Quando provido o último nome do funcionário, retorna o objeto do funcionário
+const employeeByName = (employeeName) => {
+  if (!employeeName) return {}
+  return data.employees.find(name => name.firstName === employeeName || name.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
