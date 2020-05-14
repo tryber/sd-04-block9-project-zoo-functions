69da8fac2aa38fb5e43ad15baf5db4f11d1d82c5
wistyk <leonardoxw@gmail.com>
2020-05-14 16:13:47 -0300

    esqueci de passar o rest

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   // seu código aqui
   const id = data.animals.filter(value => ids.find(index => index === value.id));
 
