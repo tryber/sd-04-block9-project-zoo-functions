c00faa3468b35d863582678953b3087c43bdd50e
binhus <eng.robsonwilli@gmail.com>
2020-05-15 16:44:24 -0300

    schedule function

--- src/zoo.js
+++ src/zoo.js
@@ -76,8 +76,23 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const days = {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED',
+  };
+  if (dayName === undefined) {
+    return days;
+  }
+  const msg = { [dayName]: days[dayName] };
+  return msg;
 }
+
+console.log(schedule('Monday'));
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
