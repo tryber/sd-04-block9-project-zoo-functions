1d78254c01567d36e799510c4ca9aa0cb57392a3
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-05-15 16:11:33 -0300

    step 10 schedule

--- src/zoo.js
+++ src/zoo.js
@@ -126,7 +126,20 @@ const animalMap = (options) => {
   return animalMapCases(options);
 };
 
-const schedule = () => {};
+const schedule = (dayName) => {
+const arr =Object.entries(data.hours);
+const newObj = arr.reduce((acc, obj) => {
+  if (obj[1].open !== 0) {
+    acc[obj[0]] = `Open from ${obj[1].open}am until ${obj[1].close -12}pm`;
+  } else {
+    acc[obj[0]] = 'CLOSED';
+  }
+  return acc;
+}, {})
+  if (dayName) {
+    return {[dayName]: newObj[dayName]};
+  } return newObj;
+};
 
 const oldestFromFirstSpecies = (id) => {};
 
