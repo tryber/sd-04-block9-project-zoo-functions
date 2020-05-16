61d5f533f2f7be00701dee281ceed029a1a6e7e5
ederusena <eder.sena@live.com>
2020-05-16 20:51:34 -0300

    Projeto Exercicio 2

--- src/zoo.js
+++ src/zoo.js
@@ -8,61 +8,16 @@ eslint no-unused-vars: [
   }
 ]
 */
+
 const data = require('./data');
 
 const animalsByIds = (...ids) =>
-  // data.animals.filter(animal => ids.find(id => id === animal.id));
-  data.animals.filter(elemento => ids.includes(elemento.id))
+  data.animals.filter(animal => ids.find(id => animal.id === id));
 
-
-
-// function animalsOlderThan(animal, age) {
-//   // seu código aqui
-// }
-
-// function employeeByName(employeeName) {
-//   // seu código aqui
-// }
-
-// function createEmployee(personalInfo, associatedWith) {
-//   // seu código aqui
-// }
-
-// function isManager(id) {
-//   // seu código aqui
-// }
-
-// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-//   // seu código aqui
-// }
-
-// function animalCount(species) {
-//   // seu código aqui
-// }
-
-// function entryCalculator(entrants) {
-//   // seu código aqui
-// }
-
-// function animalMap(options) {
-//   // seu código aqui
-// }
-
-// function schedule(dayName) {
-//   // seu código aqui
-// }
-
-// function oldestFromFirstSpecies(id) {
-//   // seu código aqui
-// }
-
-// function increasePrices(percentage) {
-//   // seu código aqui
-// }
-
-// function employeeCoverage(idOrName) {
-//   // seu código aqui
-// }
+const animalsOlderThan = (animal, age) =>
+data.animals
+  .find(animalFind => animalFind.name === animal)
+  .residents.every(especie => especie.age > age);
 
 module.exports = {
   // entryCalculator,
@@ -74,7 +29,7 @@ module.exports = {
   // employeeCoverage,
   // addEmployee,
   // isManager,
-  // animalsOlderThan,
+  animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
   // createEmployee,
