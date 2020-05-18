ca2dc53a5a112cb4220a5085fb26f063c4182a20
AliceAAtalla <aliceatalla36@gmail.com>
2020-05-18 11:25:58 -0300

    Code Climate - 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,73 +11,74 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-// TODO => 1 - Caso receba nenhum parâmetro, necessário retornar um array vazio;
-// TODO => 2 - Ao receber como parâmetro um único id, retorna os animais com este id;
-// TODO => 3 - Ao receber mais de um id, retorna os animais que têm um desses ids;
-const animalsByIds = (...ids) => {
-  return data.animals.filter(animal => ids.some(id => animal.id === id));
-}
+// 1 - Caso receba nenhum parâmetro, necessário retornar um array vazio;
+// 2 - Ao receber como parâmetro um único id, retorna os animais com este id;
+// 3 - Ao receber mais de um id, retorna os animais que têm um desses ids;
+const animalsByIds = (...ids) =>
+  data.animals.filter(animal => ids.some(id => animal.id === id));
 
-// TODO => Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada
+// Ao passar o nome de uma espécie e uma idade, testa se todos os animais
+// desta espécie possuem a idade mínima especificada
 const animalsOlderThan = (animal, age) => {
-  return data.animals.find(especie => especie.name === animal)
-    .residents.every(residente => residente.age >= age);
-}
+  data.animals.find(especie => especie.name === animal).residents
+    .every(residente => residente.age >= age);
+};
 
-// TODO => 1 - Sem parâmetros, retorna um objeto vazio
-// TODO => 2 - Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
-// TODO => 3 - Quando provido o último nome do funcionário, retorna o objeto do funcionário
+// 1 - Sem parâmetros, retorna um objeto vazio
+// 2 - Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
+// 3 - Quando provido o último nome do funcionário, retorna o objeto do funcionário
 const employeeByName = (employeeName) => {
-  if (!employeeName) return {}
-  return data.employees.find(name => name.firstName === employeeName || name.lastName === employeeName);
+  if (!employeeName) return {};
+  return data.employees
+    .find(name => name.firstName === employeeName || name.lastName === employeeName);
+};
+
+// Cria um novo colaborador a partir de objetos contendo informações pessoais,
+// gerentes e animais gerenciados
+function createEmployee(personalInfo, associatedWith) {
+  return { ...personalInfo, ...associatedWith };
 }
 
-// TODO => Cria um novo colaborador a partir de objetos contendo informações pessoais, gerentes e animais gerenciados
-const createEmployee = (personalInfo, associatedWith) => {
-  return { ...personalInfo, ...associatedWith }
-}
+// Testa se o id passado é de um gerente
+const isManager = (id) => {
+  data.employees.some(gerenteID => gerenteID.managers.find(ids => ids === id));
+};
 
-//TODO => Testa se o id passado é de um gerente
-const isManager = id => {
-  return data.employees.some(gerenteID => gerenteID.managers.find(ids => ids === id));
-}
-
-//TODO => Adiciona um funcionário no fim da lista
+// Adiciona um funcionário no fim da lista
 const addEmployee = (id, firstName, lastName, managers = [], responsibleFor = []) => {
-  return data.employees.push({ id, firstName, lastName, managers, responsibleFor })
-}
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
+};
 
-//TODO => 1 - Sem parâmetros, returna animais e suas quantidades
-//TODO => 2 - Com o nome de uma espécie de animal, retorna somente a quantidade
+// 1 - Sem parâmetros, returna animais e suas quantidades
+// 2 - Com o nome de uma espécie de animal, retorna somente a quantidade
 const animalCount = (species) => {
   if (!species) {
     return data.animals.reduce((objeto, elemento) => {
       objeto[elemento.name] = elemento.residents.length;
-      console.log(objeto)
-      return objeto
+      return objeto;
     }, {});
   }
-  return data.animals.find(element => element.name === species).residents.length
-}
+  return data.animals.find(element => element.name === species).residents.length;
+};
 
-// TODO => Returna 0 se nenhum argumento for passado
-// TODO => Retorna 0 se um objeto vazio for passado
-// TODO => Retorna o preço total a ser cobrado dado o número de adultos, crianças e idosos
+// Retorna 0 se um objeto vazio for passado
+// Retorna o preço total a ser cobrado dado o número de adultos, crianças e idosos
+// Returna 0 se nenhum argumento for passado
 const entryCalculator = (entrants = {}) => {
-  return Object.keys(entrants).reduce((acc, chave) => acc + (entrants[chave] * data.prices[chave]), 0);
-}
+  Object.keys(entrants).reduce((acc, chave) => acc + (entrants[chave] * data.prices[chave]), 0);
+};
 
 function animalMap(options) {
   // seu código aqui
 }
 
-// TODO => Sem parâmetros, retorna um cronograma legível para humanos
-// TODO => Se um único dia for passado, retorna somente este dia em um formato legível para humanos
+// Sem parâmetros, retorna um cronograma legível para humanos
+// Se um único dia for passado, retorna somente este dia em um formato legível para humanos
 
 const openOrClose = ({ open, close }) => {
-  if (open === 0 && close === 0) return 'CLOSED'; 
+  if (open === 0 && close === 0) return 'CLOSED';
   return `Open from ${open}am until ${close - 12}pm`;
-}
+};
 
 const schedule = (dayName) => {
   const objHours = data.hours;
@@ -85,26 +86,30 @@ const schedule = (dayName) => {
     return Object.keys(objHours).reduce((newObj, chave) => {
       newObj[chave] = openOrClose(objHours[chave]);
       return newObj;
-    }, {})
+    }, {});
   }
-  return { [dayName]: openOrClose(objHours[dayName]) }
-}
+  return { [dayName]: openOrClose(objHours[dayName]) };
+};
 
-//TODO => Passado o id de um funcionário, encontra a primeira espécie de animal gerenciado pelo funcionário, e retorna um array com nome, sexo e idade do animal mais velho dessa espécie
+// Passado o id de um funcionário, encontra a primeira espécie de animal gerenciado
+// pelo funcionário, e retorna um array com nome, sexo e idade do animal mais velho dessa espécie
 const oldestFromFirstSpecies = (id) => {
   const idFirstAnimal = data.employees.find(employee => employee.id === id).responsibleFor[0];
-  const valor = data.animals.find(animals => animals.id === idFirstAnimal).residents.reduce((oldestAgeAnimal, animal) => {
-    if (oldestAgeAnimal.age > animal.age) return oldestAgeAnimal;
-    return animal;
-  });
-  return [valor.name, valor.sex, valor.age]
-}
+  const valor = data.animals.find(animals => animals.id === idFirstAnimal).residents
+    .reduce((oldestAgeAnimal, animal) => {
+      if (oldestAgeAnimal.age > animal.age) return oldestAgeAnimal;
+      return animal;
+    });
+  return [valor.name, valor.sex, valor.age];
+};
 
-//TODO => Ao passar uma porcentagem, incrementa todos os preços, arrendondados em duas casas decimais
-const increasePrices = (percentage) => {
-  return Object.keys(data.prices).forEach(key => data.prices[key] =
-    Math.round(100 * ((data.prices[key] * (percentage / 100)) + data.prices[key])) / 100)
-}
+// Ao passar uma %, incrementa todos os preços, arrendondados em 2 casas decimais
+const increasePrices = percentage =>
+  Object.keys(data.prices)
+    .forEach((key) => {
+      data.prices[key] =
+        Math.round((100 * ((data.prices[key] * (percentage / 100)) + data.prices[key])) / 100);
+    });
 
 function employeeCoverage(idOrName) {
 
