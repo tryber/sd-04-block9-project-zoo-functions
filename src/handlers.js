a7b77a3dfb2c54a056f1fb52c6dac80b5b0a0a55
Ana Higo <anahigo.arq@gmail.com>
2020-05-18 02:32:28 -0300

    cansei de identificar commits

--- src/handlers.js
+++ src/handlers.js
@@ -3,7 +3,7 @@ const data = require('./data');
 const sortArray = incommingArray =>
   incommingArray.sort((a, b) => ((a.age > b.age) ? -1 : 1))[0];
 
-const getAnimals = (animalsIds) => animalsIds.map(id =>
+const getAnimals = animalsIds => animalsIds.map(id =>
   data.animals.find(animal => animal.id === id).name);
 
 module.exports = { sortArray, getAnimals };
