27649c2dfb4b518fa8d764e94892229a47d1105f
ederusena <eder.sena@live.com>
2020-05-19 14:47:28 -0300

    shedule

--- src/zoo.js
+++ src/zoo.js
@@ -60,9 +60,24 @@ const entryCalculator = (entrants) => {
   );
 };
 
+const scheduleObj = {
+  Tuesday: 'Open from 8am until 6pm',
+  Wednesday: 'Open from 8am until 6pm',
+  Thursday: 'Open from 10am until 8pm',
+  Friday: 'Open from 10am until 8pm',
+  Saturday: 'Open from 8am until 10pm',
+  Sunday: 'Open from 8am until 8pm',
+  Monday: 'CLOSED',
+};
+
+const schedule = (dayName) => {
+  if (dayName) return { [dayName]: scheduleObj[dayName] };
+  return scheduleObj;
+};
+
 module.exports = {
   entryCalculator,
-  // schedule,
+  schedule,
   animalCount,
   // animalMap,
   animalsByIds,
