88e413ae2e4460a9137992052a272d3c35ad0734
Pedro Pires <pedrotpoliveira@gmail.com>
2020-05-20 01:17:22 -0300

    Refactoring and schedule()

--- src/zoo.js
+++ src/zoo.js
@@ -9,78 +9,100 @@ eslint no-unused-vars: [
 ]
 */
 // Starting
-const data = require('./data');
-
+const data = require("./data");
 
 function animalsByIds(...ids) {
   if (!ids) return [];
   return data.animals.filter(({ id }) => ids.includes(id));
 }
 
-function animalsOlderThan(animal, age) {
-  // seu código aqui
-  const species = data.animals.find(({ name }) => name === animal);
-  const { residents } = species;
-  return residents.every(e => e.age > age);
+function animalsOlderThan(animal, idade) {
+  const { residents } = data.animals.find(({ name }) => name === animal);
+  return residents.every(({ age }) => age > idade);
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
   if (!employeeName) return {};
   return data.employees.find(
-    ({ firstName, lastName }) => firstName === employeeName || lastName === employeeName,
+    ({ firstName, lastName }) =>
+      firstName === employeeName || lastName === employeeName
   );
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
   return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
-  // seu código aqui
-  const man = data.employees.map(({ managers }) => managers).flat();
-  return man.some(e => e === id);
+  const man = data.employees.some(({ managers }) =>
+    managers.some(e => e === id)
+  );
+  return man;
 }
 
-function addEmployee(id = '', firstName = '', lastName = '', managers = [], responsibleFor = []) {
-  // seu código aqui
+function addEmployee(
+  id = "",
+  firstName = "",
+  lastName = "",
+  managers = [],
+  responsibleFor = []
+) {
   return data.employees.push({
-    id, firstName, lastName, managers, responsibleFor,
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor
   });
 }
 
 function animalCount(species) {
-  // seu código aqui
   if (!species) {
-    const allAnimals = data.animals.map(({ name, residents }) => [name, residents.length]);
-    return Object.fromEntries(allAnimals);
+    const allAnimals = {};
+    data.animals.forEach(
+      ({ name, residents }) => (allAnimals[name] = residents.length)
+    );
+    return allAnimals;
   }
-  const oneAnimal = data.animals.find(({ name }) => name === species);
-  return oneAnimal.residents.length;
+  const { residents } = data.animals.find(({ name }) => name === species);
+  return residents.length;
 }
 
 function entryCalculator(entrants) {
   // seu código aqui
-  if (!entrants) return 0;
-  if (Object.keys(entrants).length === 0) return 0;
-  const moneyArray = [data.prices].map(
-    (
-      { Adult, Senior, Child },
-    ) => [Adult * entrants.Adult, Child * entrants.Child, Senior * entrants.Senior],
-  ).flat();
-  return moneyArray.reduce((acc, cur) => acc + cur);
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const numPeople = Object.keys(entrants);
+  return numPeople.reduce(
+    (acc, key) => acc + entrants[key] * data.prices[key],
+    0
+  );
 }
-
-console.log(entryCalculator({ Adult: 2, Child: 3, Senior: 1 }));
-
 
 function animalMap(options) {
   // seu código aqui
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const humanSchedule = {};
+  if (!dayName || Object.keys(dayName) === 0) {
+    const days = Object.keys(data.hours);
+    days.forEach(day => {
+      if (day === "Monday") {
+        humanSchedule.Monday = `CLOSED`;
+      } else {
+        humanSchedule[day] = `Open from ${data.hours[day].open}am until ${data
+          .hours[day].close - 12}pm`;
+      }
+    });
+    return humanSchedule;
+  }
+  if (dayName === "Monday") {
+    humanSchedule.Monday = `CLOSED`;
+    return humanSchedule;
+  }
+  humanSchedule[dayName] = `Open from ${data.hours[dayName].open}am until ${data
+    .hours[dayName].close - 12}pm`;
+  return humanSchedule;
 }
 
 function oldestFromFirstSpecies(id) {
@@ -108,5 +130,5 @@ module.exports = {
   animalsOlderThan,
   oldestFromFirstSpecies,
   increasePrices,
-  createEmployee,
+  createEmployee
 };
