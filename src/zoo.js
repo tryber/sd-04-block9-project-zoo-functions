f1b1e2ef97dd47c99b5604d5ecc50e9c5e357038
lekaoru <lekaoru@gmail.com>
2020-05-21 12:51:22 -0300

    iniciando p rojeto. VAMOS COM TUDO :rocket:

--- src/zoo.js
+++ src/zoo.js
@@ -13,6 +13,14 @@ const data = require('./data');
 
 function animalsByIds(ids) {
   // seu código aqui
+  // Caso receba nenhum parâmetro, necessário retornar um array vazio
+  // Ao receber como parâmetro um único id, retorna os animais com este id
+  // Ao receber mais de um id, retorna os animais que têm um desses ids
+
+  function animalsByIds(...ids) {
+    if (!ids) return [];
+    const resultado = data.animals.filter(elemento => ids.includes(elemento.id));
+    return resultado;
 }
 
 function animalsOlderThan(animal, age) {
