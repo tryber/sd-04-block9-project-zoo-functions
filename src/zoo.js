c4c8f893b98cdd03552b282f5444adfc29f5c465
Cezene <cezenesn@hotmail.com.br>
2020-06-15 10:24:39 -0300

    baby steps no limite

--- src/zoo.js
+++ src/zoo.js
@@ -38,6 +38,13 @@ function isManager(id) {
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
+  return data.employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
